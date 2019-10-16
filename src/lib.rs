ba524f783b3943f67b2c694a614c292337db3b1b
mikelma <mikelma7@gmail.com>
2019-10-16 12:56:46 +0200

    Now exporting module permutation, not the struct directly. Also learnable and sampleable traits added

--- src/lib.rs
+++ src/lib.rs
@@ -3,9 +3,16 @@
 //! `permu-rs` is a collection of utilities for permutations. It contains useful 
 //!
 
-mod permutation;
-pub use permutation::Permutation;
+pub mod permutation;
 
-mod population;
-pub use population::Population;
+/// Trait for different sampleable distribution types.
+pub trait Sampleable<T> {
+    /// Samples a solution from a sampleable distribution.
+    fn sample(&self) -> Vec<T>;
+}
 
+/// Trait for learnable populations.
+pub trait Learnable<T> {
+    /// Learns a probability distribution from a population of solutions.
+    fn learn(&self) -> Sampleable<T>;
+}
