7b4a47ebd82349b2202a3c483333d6ad10593e94
mikelma <mikelma7@gmail.com>
2019-10-17 14:57:53 +0200

    Learnable and Sampleable methods changed

--- src/lib.rs
+++ src/lib.rs
@@ -5,14 +5,14 @@
 
 pub mod permutation;
 
-/// Trait for different sampleable distribution types.
-pub trait Sampleable<T> {
-    /// Samples a solution from a sampleable distribution.
-    fn sample(&self) -> Vec<T>;
+/// Trait for learnable populations.
+pub trait Learnable {
+    /// Learns a probability distribution from a population of solutions.
+    fn learn(&self) -> dyn Sampleable;
 }
 
-/// Trait for learnable populations.
-pub trait Learnable<T> {
-    /// Learns a probability distribution from a population of solutions.
-    fn learn(&self) -> Sampleable<T>;
+/// Trait for different sampleable distribution types.
+pub trait Sampleable {
+    /// Samples a solution from a sampleable distribution.
+    fn sample(&self, pop: &mut dyn Learnable) -> Result<(), &'static str>;
 }
