c434f1bd914f7e6f896bd5fdddf95e16c0000432
mikelma <mikelma7@gmail.com>
2019-10-19 13:48:30 +0200

    Learnable and Sampleable traits substituted with Population trait

--- src/lib.rs
+++ src/lib.rs
@@ -2,17 +2,11 @@
 //!
 //! `permu-rs` is a collection of utilities for permutations. It contains useful 
 //!
-
 pub mod permutation;
 
 /// Trait for learnable populations.
-pub trait Learnable {
-    /// Learns a probability distribution from a population of solutions.
-    fn learn(&self) -> dyn Sampleable;
-}
+pub trait Population {
 
-/// Trait for different sampleable distribution types.
-pub trait Sampleable {
-    /// Samples a solution from a sampleable distribution.
-    fn sample(&self, pop: &mut dyn Learnable) -> Result<(), &'static str>;
+    /// Learns a probability distribution from a population and samples another population.
+    fn sample(&self, out: &mut Self) -> Result<(), &'static str>;
 }
