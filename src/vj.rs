df6c062026074f770c9f7c8a129630fa5c994b02
mikelma <mikelma7@gmail.com>
2019-10-29 15:52:11 +0100

    Permutation library added

--- src/vj.rs
+++ src/vj.rs
@@ -2,6 +2,8 @@ use std::convert::TryFrom;
 use rand::Rng;
 use std::fmt::{Debug, Display};
 
+use crate::permutation;
+
 /// Contains a Vj vector.
 #[derive(Debug)]
 pub struct Vj<T> {
@@ -21,7 +23,48 @@ impl<T> Vj<T> where
 {
 
     /// Creates a Vj object from the vector.
-    pub fn from_vec(vec : Vec<T>) {
-        
+    ///
+    /// # Example
+    /// ```
+    /// use permu_rs::vj::Vj;
+    /// let vj_vec = vec![0,0,1,1,4];
+    /// let my_vj =  Vj::from_vec(vj_vec);
+    /// ```
+    pub fn from_vec(vec : Vec<T>) -> Vj<T> {
+        Vj { vj : vec }        
     }
+
+    /// Creates a Vj filled with 0s. 
+    ///
+    /// # Example
+    /// ```
+    /// use permu_rs::vj::Vj;
+    /// assert_eq!(vec![0,0,0], Vj::zeros(3).vj);
+    /// ```
+    pub fn zeros(lenght: usize) -> Vj<T> {
+        let mut v = Vec::with_capacity(lenght);
+        (0..lenght).for_each(|_| v.push(T::from(0u8)));
+        Vj { vj : v }
+    }
+
+    /*
+    pub fn from_permu(permu: &permutation::Permutation<T>, vj: &mut Vj<T>) -> Result<(), &'static str>{
+        
+        assert_eq!(permu.len()-1, vj.len(), "Lenght of the vj vector must be permu.len()-1");
+        
+        // Reset vj
+        for i in 0..vj.len() {
+            vj[i] = 0;
+        }
+
+        for indx in 0..permu.len() {
+            for i in indx..permu.len() {
+                if permu[i] < permu[indx] {
+                    vj[indx] += 1;
+                }            
+            }
+        }
+        Ok(())
+    } 
+    */
 }
