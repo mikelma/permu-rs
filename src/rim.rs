26b2dc15389ee2c4a569f5fd4fae8d4e6a265d94
mikelma <mikelma7@gmail.com>
2020-02-26 19:01:17 +0100

    rim's from_permu now takes the output as argument and RimPopulation created

--- src/rim.rs
+++ src/rim.rs
@@ -38,6 +38,16 @@ impl<T> Rim<T> where
         Rim { inner }        
     }
     
+    /// Creates a `Rim`vector of the length given.
+    pub fn zeros(length: usize) -> Rim<T> {
+        Rim { inner: vec![T::from(0u8); length] }
+    }
+    
+    /// Returns the length of the inner `Rim` vector.
+    pub fn len(self) -> usize {
+        self.inner.len()
+    }
+    
     /// Transforms a given insertion vector (RIM) into it's permutation representation. 
     ///
     /// # Example
@@ -45,7 +55,9 @@ impl<T> Rim<T> where
     /// use permu_rs::{ permutation::Permutation, rim::Rim };
     /// let rim = Rim::<u8>::from_vec(vec![0,2,2]);
     /// let mut output = Permutation::<u8>::identity(4);
+    ///
     /// Rim::<u8>::to_permu(&rim, &mut output).unwrap();
+    ///
     /// println!("insertion vector: {:?}", rim.inner);
     /// println!("permutation: {:?}", output.permu);
     ///
@@ -92,18 +104,23 @@ impl<T> Rim<T> where
     /// ```
     /// use permu_rs::{ permutation::Permutation, rim::Rim };
     /// let permu = Permutation::<u8>::from_vec(vec![1,0,3,2]).unwrap();
-    /// let rim = Rim::<u8>::from_permu(&permu).unwrap();
+    /// let mut rim = Rim::<u8>::zeros(3);
+    ///
+    /// Rim::<u8>::from_permu(&permu, &mut rim).unwrap();
     ///
     /// println!("permutation: {:?}", permu.permu);
     /// println!("insertion vector: {:?}", rim.inner);
     ///
-    /// let target = Rim::<u8>::from_vec(vec![0,0,2,2]);
+    /// let target = Rim::<u8>::from_vec(vec![0,2,2]);
     /// assert_eq!(target, rim);
     /// ```
-    pub fn from_permu(permu: &Permutation<T>) -> Result<Rim<T>, Error> {
+    pub fn from_permu(permu: &Permutation<T>, out: &mut Rim<T>) -> Result<(), Error> {
         let mut permu = permu.permu.clone(); // NOTE: Not efficient
         let length = permu.len();
-        let mut inner: Vec<T> = vec![T::from(0u8); length];
+        // let mut inner: Vec<T> = vec![T::from(0u8); length];
+        out.inner = out.inner.iter_mut()
+                             .map(|_| T::from(0u8))
+                             .collect();
 
         (1..length).rev()
             .for_each(|element| {
@@ -118,18 +135,43 @@ impl<T> Rim<T> where
                 let (index_t, index) = match index {
                     Some(i) => match T::try_from(i) {
                         Ok(v) => (v, i),
-                        Err(_) => unimplemented!(),
+                        Err(_) => unreachable!(),
                     },
                     None => unreachable!(),
                 };
 
                 //println!("Position of {} is {}", element, index_t);
-                inner[element] = index_t;
+                out.inner[element-1] = index_t;
 
                 permu.remove(index);
 
             });
 
-        Ok(Rim::from_vec(inner))
+        Ok(())
     }
+}
+
+/// Population of `Rim` vectors.
+#[derive(PartialEq)]
+#[derive(Debug)]
+#[derive(Clone)]
+pub struct RimPopulation<T> {
+    pub population : Vec<Rim<T>>,
+    pub size : usize,
+}
+
+impl<T> RimPopulation<T> where 
+    T : Copy +
+    From<u8> +
+    TryFrom<usize> +
+    TryInto<usize> +
+    // PartialEq<T> +
+    Eq +
+    rand::distributions::range::SampleRange +
+    std::cmp::PartialOrd +
+    std::ops::Sub +
+    // Into<usize> +
+    Display + // NOTE : For debugging
+    Debug, // NOTE : For debugging
+{
 }
